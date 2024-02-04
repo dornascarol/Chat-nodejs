87dadb319730f79fab881d5f31484824fb7b4692
dornascarol <carolinafdornas@gmail.com>
2024-02-04 20:40:40 -0300

    instalando express e socket.io

--- servidor.js
+++ servidor.js
@@ -1,2 +1,10 @@
 const http = require('http');
 const express = require('express');
+const aplicacao = express();
+
+const servidorHttp = http.createServer(aplicacao);
+const io = require('socket.io')(servidorHttp);
+
+aplicacao.use(express.static('public'));
+
+servidorHttp.listen(1000);
